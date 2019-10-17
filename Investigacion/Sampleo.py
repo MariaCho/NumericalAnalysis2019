462f294f67242d99beac6bd08a466dd00fff3500
MariaCho <mclarita2897@hotmail.com>
2019-10-17 15:54:31 -0500

    Sampleo

--- Investigacion/Sampleo.py
+++ Investigacion/Sampleo.py
@@ -2,6 +2,6 @@ from scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
 
-rate, data = wavfile.read('C:\Users\usuario\OneDrive\Documents\EAFIT\Semestre9\Analisis numerico\Investigacion\NumericalAnalysis2019\Investigacion\canciones\hakuna_matata.wav')
+rate, data = wavfile.read('canciones\hakuna_matata.wav')
 data = data[5000000:5000100] 
 print(data)
